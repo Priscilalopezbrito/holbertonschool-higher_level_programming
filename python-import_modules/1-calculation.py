9ff111d357be1ff4e74e730bc2281184b70e78b4
Priscila Lopez <prisscilalb@gmail.com>
2024-09-05 15:09:13 -0400

    Adding if __name__ == __main__: to file 1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-a = 10
-b = 5
-suma = add(a, b)
-print("{} + {} = {}".format(a, b, suma))
-resta = sub(a, b)
-print("{} - {} = {}".format(a, b, resta))
-multi = mul(a, b)
-print("{} * {} = {}".format(a, b, multi))
-divi = div(a, b)
-print("{} / {} = {}".format(a, b, divi))
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    suma = add(a, b)
+    print("{} + {} = {}".format(a, b, suma))
+    resta = sub(a, b)
+    print("{} - {} = {}".format(a, b, resta))
+    multi = mul(a, b)
+    print("{} * {} = {}".format(a, b, multi))
+    divi = div(a, b)
+    print("{} / {} = {}".format(a, b, divi))
