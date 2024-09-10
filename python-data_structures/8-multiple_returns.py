03385c7d59c2adcad9a4a28a9dd599772cff3c36
Priscila Lopez <prisscilalb@gmail.com>
2024-09-10 11:36:42 -0400

    remove declarations and fix return

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    first = sentence[0]
-    lenght = len(sentence)
     if sentence == "":
         return (0, None)
     else:
-        return (lenght, first)
+        return (len(sentence), sentence[0])
