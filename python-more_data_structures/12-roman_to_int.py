5bb957ef49f33d9e5e1f29c852b1874302700130
Priscila Lopez <prisscilalb@gmail.com>
2024-09-12 11:34:06 -0400

    Adding condition to calculate cases like IV

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,12 @@
 def roman_to_int(roman_string):
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     integer = 0
+    prev = 0
     for i in roman_string:
-        if i in roman:
-            integer += roman[i]
+        current = roman[i]
+        if prev < current:
+            integer += current - 2 * prev
+        else:
+            integer += current
+        prev = current
     return integer
