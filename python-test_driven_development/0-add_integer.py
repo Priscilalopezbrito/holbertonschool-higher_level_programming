f6664e23b6b517496ec5ba1dd11f861aeecd68eb
Priscila Lopez <prisscilalb@gmail.com>
2024-09-12 18:44:43 -0400

    Adding code in function and documentation file 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,5 +5,22 @@ Module contains function that adds two integers.
 
 
 def add_integer(a, b=98):
+    """
+    Calculate sum of a two integers/floats a and b.
 
+    Parameters:
+        a (int, float): First number in sum
+        b (int, float): Second number in sum
+
+    Returns:
+        int: Sum of the two numbers
+    Raises:
+        TypeError: If 'a' or 'b' are not integers or floats
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
     return a + b
