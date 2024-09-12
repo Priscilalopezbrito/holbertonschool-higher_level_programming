4bfb5ea241ca19c7e4e96a08cb2241e096ad6169
Priscila Lopez <prisscilalb@gmail.com>
2024-09-12 11:21:49 -0400

    Adding file 12-roman_to_int.py

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    integer = 0
+    for i in roman_string:
+        if i in roman:
+            integer += roman[i]
+    return integer
