7cbefea26b87aecf5b1c0949287a1e42168a600b
Priscila Lopez <prisscilalb@gmail.com>
2024-09-12 20:19:51 -0400

    File 3

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module contains function that that prints My name is <first name> <last name>
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints My name is <first name> <last name>
+    Parameters:
+        first_name (str): string
+        last_name (str): string
+    Raises:
+        TypeError: If first_name or last_name is not a string.
+    """
