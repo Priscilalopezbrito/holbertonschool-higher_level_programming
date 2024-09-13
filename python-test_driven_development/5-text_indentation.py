59ce01d4f897f7463fd49f6ff518f1f28057e385
Priscila Lopez <prisscilalb@gmail.com>
2024-09-12 22:07:08 -0400

    5-text_indentation.py

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module contains function that prints a text with 2 new lines
+after each of these characters: ., ? and :
+"""
+
+
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines
+
+    Parameters:
+        text (str): Text string
+    Raises:
+        TypeError: text must be a string
+    """
