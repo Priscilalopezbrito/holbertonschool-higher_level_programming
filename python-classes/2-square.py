341f51b29fcc69d184946deb2d734723a30a22aa
Priscila Lopez <prisscilalb@gmail.com>
2024-09-16 11:51:56 -0400

    Adding file 2-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module defines an empty Square class.
+"""
+
+
+class Square:
+    """
+    Represents a square.
+    """
+    def __init__(self, size=0):
+        """
+        Initialize a Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
