22bf202177535bf89ae6f833a9ab8511e0f473fd
Priscila Lopez <prisscilalb@gmail.com>
2024-09-16 11:27:35 -0400

    Adding file 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module defines an empty Square class.
+"""
+
+
+class Square:
+    """
+    Represents a square.
+    """
+    def __init__(self, size):
+        """
+        Initialize a Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
