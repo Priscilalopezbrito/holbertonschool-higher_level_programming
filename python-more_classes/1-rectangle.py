ed12348147895b451b7ee1527408e6d0d0f32a99
Priscila Lopez <prisscilalb@gmail.com>
2024-09-18 13:45:15 -0400

    Fix error msg

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -40,9 +40,9 @@ class Rectangle:
             ValueError: If height is less than 0.
         """
         if not isinstance(value, int):
-            raise TypeError("height must be an integer")
+            raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -66,7 +66,7 @@ class Rectangle:
             ValueError: If height is less than 0.
         """
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
