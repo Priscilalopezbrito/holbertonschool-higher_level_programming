c24435db2063df3097ae0e1c18a855727fb03252
Priscila Lopez <prisscilalb@gmail.com>
2024-09-23 10:00:36 -0400

    Adding documentation

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+This module returns the list of available
+attributes and methods of an object.
+"""
+
+
 def lookup(obj):
     """
     Returns list of available attributes
