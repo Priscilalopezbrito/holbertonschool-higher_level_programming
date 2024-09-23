7c6905dda7a820654530a7ea03fa97765f9a8ab4
Priscila Lopez <prisscilalb@gmail.com>
2024-09-23 17:16:32 -0400

    change order

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -27,7 +27,7 @@ class Rectangle(BaseGeometry):
     Class Rectangle inherits from BaseGeometry
     """
     def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
         super().integer_validator("width", width)
         super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
