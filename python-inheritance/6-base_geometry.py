f5b27b30303dec4b416a8a45e0e5371f316e5b38
Priscila Lopez <prisscilalb@gmail.com>
2024-09-23 14:35:24 -0400

    Adding file 6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Class BaseGeometry
+Improve Geometry
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
