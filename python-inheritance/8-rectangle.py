78c32dbc11bc48d32f2b028826081b92b8865f84
Priscila Lopez <prisscilalb@gmail.com>
2024-09-23 17:33:46 -0400

    change order

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -5,22 +5,8 @@ Improve Geometry
 """
 
 
-class BaseGeometry:
-    """
-    BaseGeometry class
-    """
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        that validates value
-        """
-        if not isinstance(value, int) or isinstance(value, bool):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""superclass import """
 
 class Rectangle(BaseGeometry):
     """
