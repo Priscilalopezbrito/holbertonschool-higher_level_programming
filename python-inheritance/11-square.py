1a84a1a4e53f15ab2336f1237fbfca87cdbcf06a
Priscila Lopez <prisscilalb@gmail.com>
2024-09-24 09:48:55 -0400

    Adding file 11-square.py

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Class BaseGeometry
+Class Rectangle
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+"""superclass import """
+
+
+class Square(Rectangle):
+    """
+    Class Square
+    """
+
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return (self.__size)**2
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
