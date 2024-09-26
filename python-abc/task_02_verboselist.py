89c5152c5360a06eb617acb6f4c7a4f3591ce0b3
Priscila Lopez <prisscilalb@gmail.com>
2024-09-26 13:47:21 -0400

    Adding file task_02_verboselist.py

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Extending the Python List with Notifications
+"""
+
+
+class VerboseList(list):
+    """
+    Class VerboseList extends the Python list class
+    """
+    def append(self, item):
+        super().append(item)
+        print("Added [{}] to the list.".format(item))
+
+    def extend(self, iterable):
+        super().extend(iterable)
+        print("Extended the list with [{}] items.".format(len(iterable)))
+
+    def remove(self, item):
+        super().remove(item)
+        print("Removed [{}] from the list.".format(item))
+
+    def pop(self, index=-1):
+        item = super().pop(index)
+        print("Popped [{}] from the list.".format(item))
+        return item
