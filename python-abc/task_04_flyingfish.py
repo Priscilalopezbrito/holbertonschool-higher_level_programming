4ea214ae7e56b0b0dd9e6fbf60549f29cc41da0d
Priscila Lopez <prisscilalb@gmail.com>
2024-09-26 16:49:41 -0400

    Adding file task_04_flyingfish.py

---
+++ python-abc/task_04_flyingfish.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+The Enigmatic FlyingFish - Exploring Multiple Inheritance
+"""
+
+
+class Fish:
+    """
+    Fish class with methods swim and habitat.
+    """
+    def swim(self):
+        print("The fish is swimming")
+
+    def habitat(self):
+        print("The fish lives in water")
+
+
+class Bird:
+    """
+    Bird class with methods fly and habitat.
+    """
+    def fly(self):
+        print("The bird is flying")
+
+    def habitat(self):
+        print("The bird lives in the sky")
+
+
+class FlyingFish(Fish, Bird):
+    """
+    FlyingFish class that inherits from both Fish and Bird.
+    """
+    def fly(self):
+        print("The flying fish is soaring!")
+
+    def swim(self):
+        print("The flying fish is swimming!")
+
+    def habitat(self):
+        print("The flying fish lives both in water and the sky!")
