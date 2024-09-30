666fb7c00db50444c54d7532be5b9ac1c9a4c270
Priscila Lopez <prisscilalb@gmail.com>
2024-09-30 10:02:39 -0400

    Adding 'r' and end=

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -12,6 +12,6 @@ def read_file(filename=""):
     Args:
         filename (str): File name.
     """
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, 'r', encoding="utf-8") as f:
         read_data = f.read()
-        print(read_data)
+        print(read_data, end="")
