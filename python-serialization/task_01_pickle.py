324f1abd51ec20bbed06d6827f67eb1bb569f801
Priscila Lopez <prisscilalb@gmail.com>
2024-10-03 11:31:22 -0400

    Testing file task_01_pickle.py

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/ env python3
+import pickle
+"""
+Serialize and deserialize custom Python
+objects using the pickle module.
+"""
+
+
+class CustomObject:
+    """
+    Class named CustomObject
+    """
+    def __init__(self, name, age, is_student):
+        """
+        init
+        """
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """
+        Print out the object’s attributes.
+        """
+        print('Name: {}'.format(self.name))
+        print('Age: {}'.format(self.age))
+        print('Is Student: {}'.format(self.is_student))
+
+    def serialize(self, filename):
+        """
+        Serialize the current instance of the object
+        and save it to the provided filename.
+        """
+        with open(filename, 'wb') as f:
+            pickle.dump(self, f)
+
+    @classmethod
+    def deserialize(cls, filename):
+        """
+        Using the pickle module, it will load and
+        return an instance of the CustomObject
+        from the provided filename.
+        """
+        with open(filename, 'rb') as f:
+            obj = pickle.load(f)
+            return obj
