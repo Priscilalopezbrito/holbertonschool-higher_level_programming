d3f6b07361b5b7582b96fbe7ad91ee3b0facd22d
Priscila Lopez <prisscilalb@gmail.com>
2024-10-03 14:46:47 -0400

    Adding file 2

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Converting CSV Data to JSON Format
+"""
+import csv
+import json
+
+
+def convert_csv_to_json(csv_file, json_file='data.json'):
+    """
+    Takes the CSV filename as its parameter
+    and writes the JSON data to data.json.
+    """
+    with open(csv_file, 'r',  encoding='utf-8') as csvf:
+        csvR = csv.DictReader(csvf)
+        data = list(csvR)
+
+    with open(json_file, 'w', encoding='utf-8') as jsonf:
+        jsonf.write(json.dumps(data, indent=4))
