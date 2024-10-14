3100c5053c298be80ec92578b577895e5e7c9800
priscilalopez <prisscilalb@gmail.com>
2024-10-14 17:35:30 -0400

    this is a test from pycharm again

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    #  Print the status code of the response
+    print("Status Code: {}".format(response.status_code))
+
+    #  If successful, parse the fetched data into a JSON object
+    if response.status_code == 200:
+        posts = response.json()
+        if posts:
+            #  Iterate through JSON data and print the titles of all posts
+            for post in posts:
+                print(post["title"])
+
+    else:
+        print("Failed to fetch posts")
+
+
+def fetch_and_save_posts():
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+
+    if response.status_code == 200 and response.json():
+        posts = response.json()
+        with open("posts.csv", "w", newline="", encoding="utf-8") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=["userId", "id", "title", "body"])
+            writer.writeheader()
+            writer.writerows(posts)
+    else:
+        print("Failed to fetch posts")
