9a11c5cf70f00d2c842d33f5daa8c84def1aa712
priscilalopez <prisscilalb@gmail.com>
2024-10-17 18:53:18 -0400

    Adding app.config['SECRET_KEY'] = 'secret'

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -5,8 +5,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
-
-
+app.config['SECRET_KEY'] = 'secret'
 app.config['JWT_SECRET_KEY'] = 'super-secret-key'
 jwt = JWTManager(app)
 
