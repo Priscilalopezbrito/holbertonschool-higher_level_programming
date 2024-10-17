ce72c20d8a2745ec4b362e56ccbc59a9f1d0530f
priscilalopez <prisscilalb@gmail.com>
2024-10-17 19:44:05 -0400

    testing previous login

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -41,18 +41,19 @@ def basic_protected():
     return "Basic Auth: Access Granted"
 
 
-app.route('/login', methods=['POST'])
+@app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
     username = data.get('username')
     password = data.get('password')
-
     user = users.get(username)
-    if not user or not check_password_hash(user['password'], password):
-        return jsonify({"error": "Invalid credentials"}), 401
+    if user and check_password_hash(user['password'], password):
+        access_token = create_access_token(
+            identity={"username": username, "role": user['role']}
+        )
+        return jsonify({"access_token": access_token}), 200
 
-    access_token = create_access_token(identity={"username": username, "role": user["role"]})
-    return jsonify(access_token=access_token), 200
+    return jsonify({"error": "Invalid credentials"}), 401
 
 
 
