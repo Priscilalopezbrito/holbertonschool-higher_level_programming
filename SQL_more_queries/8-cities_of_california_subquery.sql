e8ff9e7e7b055471370088da2ab792de87f3d8d0
priscilalopez <prisscilalb@gmail.com>
2024-10-22 08:27:46 -0400

    Adding file 8-cities_of_california_subquery.sql

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+-- Cities of California
+-- Script that lists cities of California found in database hbtn_0d_usa
+SELECT id, name
+FROM cities
+WHERE state_id = (
+SELECT id FROM states WHERE name = 'California'
+)
+ORDER BY id ASC;
