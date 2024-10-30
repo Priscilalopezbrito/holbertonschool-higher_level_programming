2868318deda53d638574f9136086914dfffd75cc
priscilalopez <prisscilalb@gmail.com>
2024-10-30 11:47:27 -0400

    remove = symbol from query in file 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
                          db=database
                          )
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY = '{}'".format(state_search))
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'".format(state_search))
 
     rows = cursor.fetchall()
     for row in rows:
