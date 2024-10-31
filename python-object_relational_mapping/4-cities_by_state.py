250092d154c6886333c3b87f1043a49e90431ccc
priscilalopez <prisscilalb@gmail.com>
2024-10-31 15:15:40 -0400

    Fix indent line 17 pycodestyle

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
                          user=sys.argv[1],
                          passwd=sys.argv[2],
                          db=sys.argv[3]
-    )
+                         )
 
     cursor = db.cursor()
     query = """
