6177cee636de7fd43b7bb61f66fe2a2999880de9
priscilalopez <prisscilalb@gmail.com>
2024-10-31 10:00:34 -0400

    ASC

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+
 """
 Filter states by user input
 """
+
 import sys
 import MySQLdb
 
 if __name__ == '__main__':
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
@@ -17,6 +20,7 @@ if __name__ == '__main__':
                          passwd=password,
                          db=database
                          )
+
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC  "
                    .format(state_search))
