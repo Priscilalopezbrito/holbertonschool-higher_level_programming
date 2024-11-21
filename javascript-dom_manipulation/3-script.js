4d57095478fa1b78d74c1cd853d168754e150f54
priscilalopez <prisscilalb@gmail.com>
2024-11-21 15:10:27 -0400

    Adding js file 3-script.js

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleHeader = document.getElementById('toggle_header');
+  const header = document.querySelector('header');
+
+  toggleHeader.addEventListener('click', () => {
+    header.classList.toggle('red');
+    header.classList.toggle('green');
+  });
+});
