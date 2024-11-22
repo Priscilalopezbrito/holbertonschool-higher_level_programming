1093676e4edf09e1482a224eef1523cbd68e8b05
priscilalopez <prisscilalb@gmail.com>
2024-11-22 15:34:17 -0400

    Adding files 6-script.js and 7-script.js

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById('character').textContent = data.name;
+    });
+});
