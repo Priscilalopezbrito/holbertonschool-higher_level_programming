888b00c53e48991a241028f98bbec852651e9d39
priscilalopez <prisscilalb@gmail.com>
2024-11-22 15:45:50 -0400

    Adding files 6-script.js and 8-script.js

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('https://hellosalut.stefanbohacek.dev/?lang=fr')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("hello").textContent = data.hello;
+        });
+});
